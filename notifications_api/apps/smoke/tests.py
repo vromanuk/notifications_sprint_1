dcb4f200424988f8bd5580d4d2805edcf6435c4a
Vlad Romaniuk <romanuk_vlad@ukr.net>
2021-12-15 11:41:46 +0200

    Fix CI

--- notifications_api/apps/smoke/tests.py
+++ notifications_api/apps/smoke/tests.py
@@ -5,5 +5,7 @@ from django.urls import reverse
 
 def test_smoke(client):
     url = reverse("smoke")
-    response = client.get(url)
+    response = client.get(url, format="json")
+    response_json = response.json()
     assert response.status_code == http.HTTPStatus.OK
+    assert response_json == {"msg": "OK"}
--- notifications_api/apps/smoke/views.py
+++ notifications_api/apps/smoke/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
 from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
 
 
 class Smoke(GenericAPIView):
     def get(self, request):
-        return HttpResponse({"msg": "OK"})
+        return Response({"msg": "OK"})
