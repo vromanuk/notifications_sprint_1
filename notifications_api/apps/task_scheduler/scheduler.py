eef12a488c5a4fa697916c4e061e98b92375e52c
Vlad Romaniuk <romanuk_vlad@ukr.net>
2021-12-20 22:50:23 +0200

    Properly set up name for scheduler

--- notifications_api/apps/task_scheduler/scheduler.py
+++ notifications_api/apps/task_scheduler/scheduler.py
@@ -1,12 +1,17 @@
 from django.utils import timezone
 
 from notifications_api.apps.task_scheduler.models import Task
+from notifications_api.apps.task_scheduler.utils import object_to_fully_qualified_name
 
 
 class Scheduler:
-    def __init__(self, handle, params):
-        self.handle = handle
-        self.params = params
+    def __init__(self, handle=None, params=None):
+        if handle:
+            self.handle = object_to_fully_qualified_name(handle)
+        if not params:
+            self.params = dict()
+        else:
+            self.params = params
 
     def create_task(self, launched_at):
         return Task.objects.create(
@@ -15,5 +20,5 @@ class Scheduler:
 
     def cancel_task(self):
         return Task.objects.filter(handle=self.handle, state=Task.State.PENDING).update(
-            state=Task.State.CANCELED, finished_at=timezone.now()
+            handle=self.handle, state=Task.State.CANCELED, finished_at=timezone.now()
         )
